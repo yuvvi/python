02d4e147b7180d29cc263b83f5fb4aa08f28e8e5
yuvaram <yuvaram.aligeti@gmail.com>
2018-09-09 19:29:52 +0530

    Update 6_functions.py

--- 6_functions.py
+++ 6_functions.py
@@ -7,13 +7,32 @@
 #############################################################################
 
 def fun1(var1, var2):
-	print '-----fun1-----start'
-	print 'var1 :', var1
-	print 'var2 :', var2
-	print '-----fun1-----end'
+	print '\n-----fun1-----start'
+	print '\nvar1 :', var1
+	print '\nvar2 :', var2
+	print '\n-----fun1-----end\n'
+
+def add(a, b):
+	print '\n-----add-----start'
+	print '\nADD %d + %d' %(a, b)
+	print '\n-----add-----end'
+	return a + b
+
+def substract(a, b):
+	print '\n-----substract-----start'
+	print '\nsubstract %d - %d' %(a, b)
+	print '\n-----substract-----end'
+	return a - b
+
+def divide(a, b):
+	print '\n-----divide-----start'
+	print '\nDivide %d + %d' %(a, b)
+	print '\n-----divide-----end'
+	return a / b
 
 
 print '__MAIN__'
 fun1(10, 20)
 fun1(10+20, 20+30)
-
+print '\n: ',add(10,20)
+print '\n: ',substract(10,20)
