c0b76a464dced46d8e51db0231055120eecc1337
yuvaram <yuvaram.aligeti@gmail.com>
2018-09-09 19:09:25 +0530

    Create functions.py
    functions

---
+++ functions.py
@@ -0,0 +1,19 @@
+#############################################################################
+#
+#	Author		:	Yuvaram Aligeti				                             
+#	Date		:	9th, Sep 2018				                                 
+#	Description	:	Functions
+#
+#############################################################################
+
+def fun1(var1, var2):
+	print '-----fun1-----start'
+	print 'var1 :', var1
+	print 'var2 :', var2
+	print '-----fun1-----end'
+
+
+print '__MAIN__'
+fun1(10, 20)
+fun1(10+20, 20+30)
+
