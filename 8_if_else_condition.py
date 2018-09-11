00dc078b626d52ce38dbdd3e44e7fd96688b3d96
yuvaram <yuvaram.aligeti@gmail.com>
2018-09-11 17:45:31 +0530

    Create 8_if_else_condition.py

---
+++ 8_if_else_condition.py
@@ -0,0 +1,19 @@
+#############################################################################
+#
+#	Author		:	Yuvaram Aligeti	
+#	Date		:	11th, Sep 2018
+#	Description	:	IF ELSE condition
+#
+#############################################################################
+
+people = 30
+cars = 40
+
+if cars > people:
+	print "too many cars than people. "
+elif cars < people:
+	print "Less car count compared to people"
+else:
+	print "equal number of cars & people"
+  
+  
