60598d3c4584319a2ce6215d9a44d5dcef19959a
yuvaram <yuvaram.aligeti@gmail.com>
2018-09-11 17:38:38 +0530

    Update 7_logical_operations.py

--- 7_logical_operations.py
+++ 7_logical_operations.py
@@ -1,1 +1,29 @@
+#############################################################################
 #
+#	Author		:	Yuvaram Aligeti	
+#	Date		:	11th, Sep 2018
+#	Description	:	Logical operations
+#
+#############################################################################
+'''
+  AND         - and
+  OR          - or
+  NOT         - not
+  NOT EQUAL   - !=
+  EQUAL       - =
+  GREATER-THAN-EQUAL  - >=
+  LESS-THAN-EQUAL     - <=
+  TRUE                - True
+  FALSE               - False
+'''
+
+a_var = 10
+b_var = 20
+
+print 'A_var AND b_var : ',a_var==10 and b_var ==30
+print 'A_var OR b_var : ',a_var==10 or b_var ==30
+print 'A_var greater-than-equal b_var : ',a_var >= b_var
+print 'A_var lessthan-than-equal b_var : ',a_var <= b_var
+print 'A_var not equal b_var : ',a_var != b_var
+print 'word compare AND Vs and :', "AND" == "and"
+
